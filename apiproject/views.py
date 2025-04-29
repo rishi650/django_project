1479d162261da07539dca7eaa1c163c0fc27d47c
rishi <rishimahadev00@gmail.com>
2025-04-29 14:12:48 +0530

    [delete]

--- apiproject/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def root(request):
-     response =  "please register to access the products api"
-     return HttpResponse(response)
-
-     
--- crud_api_app/serializers.py
+++
@@ -1,9 +0,0 @@
-# serializers.py
-
-from rest_framework import serializers
-from .models import Products
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Products
-        fields = '__all__' 
